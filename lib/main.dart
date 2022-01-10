cd327132198460893cda013647eedd57fbe03acd
NDCrocker <59457029+NDCrocker@users.noreply.github.com>
2022-01-10 16:54:54 -0700

    successfully add Scroll functionality to the setup menus

--- lib/main.dart
+++ lib/main.dart
@@ -142,25 +142,86 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class SetupMenu extends StatelessWidget {
+// class SetupMenu extends StatelessWidget {
+//   const SetupMenu({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // return DefaultTabController(
+//     //     length: 2,
+//     //     child: Scaffold(
+//     //       resizeToAvoidBottomInset: false,
+//     //       appBar: AppBar(
+//     //           title: const Text("Setup Menu"),
+//     //           bottom: const TabBar(tabs: [
+//     //             Tab(icon: Icon(Icons.wifi), text: "WiFi Menu"),
+//     //             Tab(icon: Icon(Icons.auto_graph), text: "ThingSpeak Menu")
+//     //           ])),
+//     //       body: const TabBarView(children: <Widget>[
+//     //         WiFiPage(),
+//     //         ThingSpeakPage(),
+//     //       ]),
+//     //     ));
+//     return
+//   }
+// }
+
+class SetupMenu extends StatefulWidget {
   const SetupMenu({Key? key}) : super(key: key);
 
   @override
+  _SetupMenuState createState() => _SetupMenuState();
+}
+
+class _SetupMenuState extends State<SetupMenu>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late ScrollController _scrollViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+    _scrollViewController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _scrollViewController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-              title: const Text("Setup Menu"),
-              bottom: const TabBar(tabs: [
-                Tab(icon: Icon(Icons.wifi), text: "WiFi Menu"),
-                Tab(icon: Icon(Icons.auto_graph), text: "ThingSpeak Menu")
-              ])),
-          body: const TabBarView(children: <Widget>[
-            WiFiPage(),
-            ThingSpeakPage(),
-          ]),
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: NestedScrollView(
+          controller: _scrollViewController,
+          headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: const Text('Setup Menu'),
+                pinned: true,
+                floating: true,
+                forceElevated: boxIsScrolled,
+                bottom: TabBar(
+                  tabs: const [
+                    Tab(icon: Icon(Icons.wifi), text: "WiFi Menu"),
+                    Tab(icon: Icon(Icons.auto_graph), text: "ThingSpeak Menu")
+                  ],
+                  controller: _tabController,
+                ),
+              )
+            ];
+          },
+          body: TabBarView(
+            children: const <Widget>[
+              WiFiPage(),
+              ThingSpeakPage(),
+            ],
+            controller: _tabController,
+          ),
         ));
   }
 }
@@ -183,7 +244,7 @@ class _WiFiPageState extends State<WiFiPage> {
         alignment: Alignment.topCenter,
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
@@ -193,26 +254,38 @@ class _WiFiPageState extends State<WiFiPage> {
                     style: GoogleFonts.montserrat(
                       fontSize: 24,
                     ))),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'SSID'),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'SSID'),
+              ),
             ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Username'),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Username'),
+              ),
             ),
-            const TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: const TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+              ),
             ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: buttonRoundBorder,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 100.0)),
-                onPressed: () {},
-                child:
-                    Text('Save', style: Theme.of(context).textTheme.headline4!))
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 40.0),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: buttonRoundBorder,
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16.0, horizontal: 100.0)),
+                  onPressed: () {},
+                  child: Text('Save',
+                      style: Theme.of(context).textTheme.headline4!)),
+            )
           ],
         ),
       ),
