5f9d97690411ed93f08fc094bff7659882578dfd
Nicolas Crocker <ncrocker@ualberta.ca>
2022-01-21 17:20:39 -0700

    Creating bluetooth.dart for bluetooth functionality and local ble connect page

---
+++ lib/bluetooth.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+import 'package:flutter_blue/flutter_blue.dart';
+
+class BLEConnect extends StatelessWidget {
+  const BLEConnect({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
