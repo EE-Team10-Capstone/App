87a736b17b7dfc0e834e0b23f0f2284dd280576f
Nicolas Crocker <ncrocker@ualberta.ca>
2022-01-29 19:07:40 -0700

    Completed Textfield validation for the setup menus via Form. Work much be done to reformat pages

--- lib/thingspeak.dart
+++ lib/thingspeak.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:url_launcher/url_launcher.dart';
+import 'package:regexpattern/regexpattern.dart';
 
 _launchURL() async {
   const String url = 'https://thingspeak.com/login?skipSSOCheck=true';
@@ -23,6 +24,8 @@ class _ThingSpeakState extends State<ThingSpeakPage> {
   RoundedRectangleBorder buttonRoundBorder = const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(Radius.circular(20)));
 
+  final _TSKey = GlobalKey<FormState>();
+
   //Creating TextEditingControllers
   late TextEditingController _WRAPIcontroller;
   late TextEditingController _RDAPIcontroller;
@@ -38,6 +41,11 @@ class _ThingSpeakState extends State<ThingSpeakPage> {
   String channelTemp = '2';
   String channelRH = '3';
 
+  //Defining ThingSpeak Credential max lengths
+  int APImaxlength = 16;
+  int ChannelIDmaxlength = 7;
+
+  @override
   void initState() {
     super.initState();
     _WRAPIcontroller = TextEditingController();
@@ -52,138 +60,160 @@ class _ThingSpeakState extends State<ThingSpeakPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                padding: const EdgeInsets.all(8.0),
-                margin: const EdgeInsets.symmetric(vertical: 8.0),
-                alignment: Alignment.center,
-                child: Text(
-                    'Please insert your ThingSpeakPage Channel Credentials below',
-                    style: Theme.of(context).textTheme.headline6!)),
-            ElevatedButton.icon(
-              icon: const Icon(Icons.outbond),
+    return Form(
+      key: _TSKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.symmetric(vertical: 8.0),
+              alignment: Alignment.center,
+              child: Text(
+                  'Please insert your ThingSpeakPage Channel Credentials below',
+                  style: Theme.of(context).textTheme.headline6!)),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.outbond),
+            style: ElevatedButton.styleFrom(
+                shape: buttonRoundBorder,
+                padding: const EdgeInsets.symmetric(
+                    vertical: 16.0, horizontal: 100.0)),
+            onPressed: _launchURL,
+            label: Text('New Channel',
+                style: Theme.of(context).textTheme.headline6!),
+          ),
+          TextFormField(
+            controller: _WRAPIcontroller,
+            maxLength: APImaxlength,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Write API Key'),
+            validator: (text) {
+              if (text == null || text.length != APImaxlength) {
+                return 'Please Enter the Correct Write API Key';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _RDAPIcontroller,
+            maxLength: APImaxlength,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Read API Key'),
+            validator: (text) {
+              if (text == null || text.length != APImaxlength) {
+                return 'Please Enter the Correct Read API Key';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: _CHANIDcontroller,
+            maxLength: ChannelIDmaxlength,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Channel ID'),
+            validator: (text) {
+              if (text == null ||
+                  text.length != ChannelIDmaxlength ||
+                  !text.isNumeric()) {
+                return 'Please Enter the Correct Channel ID';
+              }
+              return null;
+            },
+          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Icon(Icons.biotech),
+                Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.all(5.0),
+                    child: const Text('CO2')),
+                const Icon(Icons.thermostat),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(5.0),
+                  child: const Text('Temperature'),
+                ),
+                const Icon(Icons.water),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(5.0),
+                  child: const Text('Humidity'),
+                ),
+              ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                child: DropdownButton<String>(
+                  alignment: Alignment.center,
+                  value: channelCO2,
+                  items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value, child: Text(value));
+                  }).toList(),
+                  onChanged: (String? newchannelCO2) {
+                    setState(() {
+                      channelCO2 = newchannelCO2!;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                child: DropdownButton<String>(
+                  alignment: Alignment.center,
+                  value: channelTemp,
+                  items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value, child: Text(value));
+                  }).toList(),
+                  onChanged: (String? newchannelTemp) {
+                    setState(() {
+                      channelTemp = newchannelTemp!;
+                    });
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 48.0),
+                child: DropdownButton<String>(
+                  alignment: Alignment.center,
+                  value: channelRH,
+                  items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
+                      .map((String value) {
+                    return DropdownMenuItem<String>(
+                        value: value, child: Text(value));
+                  }).toList(),
+                  onChanged: (String? newchannelRH) {
+                    setState(() {
+                      channelRH = newchannelRH!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+          ElevatedButton(
               style: ElevatedButton.styleFrom(
                   shape: buttonRoundBorder,
                   padding: const EdgeInsets.symmetric(
                       vertical: 16.0, horizontal: 100.0)),
-              onPressed: _launchURL,
-              label: Text('New Channel',
-                  style: Theme.of(context).textTheme.headline5!),
-            ),
-            TextField(
-              controller: _WRAPIcontroller,
-              maxLength: 16,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Write API Key'),
-            ),
-            TextField(
-              controller: _RDAPIcontroller,
-              maxLength: 16,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Read API Key'),
-            ),
-            TextField(
-              controller: _CHANIDcontroller,
-              maxLength: 7,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Channel ID'),
-            ),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(Icons.biotech),
-                  Container(
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.all(5.0),
-                      child: const Text('CO2')),
-                  const Icon(Icons.thermostat),
-                  Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(5.0),
-                    child: const Text('Temperature'),
-                  ),
-                  const Icon(Icons.water),
-                  Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(5.0),
-                    child: const Text('Humidity'),
-                  ),
-                ]),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: DropdownButton<String>(
-                    alignment: Alignment.center,
-                    value: channelCO2,
-                    items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
-                        .map((String value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (String? newchannelCO2) {
-                      setState(() {
-                        channelCO2 = newchannelCO2!;
-                      });
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: DropdownButton<String>(
-                    alignment: Alignment.center,
-                    value: channelTemp,
-                    items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
-                        .map((String value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (String? newchannelTemp) {
-                      setState(() {
-                        channelTemp = newchannelTemp!;
-                      });
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                  child: DropdownButton<String>(
-                    alignment: Alignment.center,
-                    value: channelRH,
-                    items: <String>['1', '2', '3', '4', '5', '6', '7', '8']
-                        .map((String value) {
-                      return DropdownMenuItem<String>(
-                          value: value, child: Text(value));
-                    }).toList(),
-                    onChanged: (String? newchannelRH) {
-                      setState(() {
-                        channelRH = newchannelRH!;
-                      });
-                    },
-                  ),
-                ),
-              ],
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    shape: buttonRoundBorder,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 16.0, horizontal: 100.0)),
-                onPressed: () {},
-                child:
-                    Text('Save', style: Theme.of(context).textTheme.headline4!))
-          ],
-        ),
+              onPressed: () {
+                if (_TSKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child:
+                  Text('Save', style: Theme.of(context).textTheme.headline4!))
+        ],
       ),
     );
   }
--- lib/wifi.dart
+++ lib/wifi.dart
@@ -3,6 +3,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:regexpattern/regexpattern.dart';
+
 class WiFiPage extends StatefulWidget {
   const WiFiPage({Key? key}) : super(key: key);
 
@@ -14,14 +16,11 @@ class _WiFiPageState extends State<WiFiPage> {
   RoundedRectangleBorder buttonRoundBorder = const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(Radius.circular(20)));
 
+  final _WifiKey = GlobalKey<FormState>();
+
   late TextEditingController _SSIDcontroller;
   late TextEditingController _USERIDcontroller;
   late TextEditingController _PASSWDcontroller;
-
-  //Initializing string values for text values
-  String ssid = '';
-  String userid = '';
-  String password = '';
 
   @override
   void initState() {
@@ -34,62 +33,76 @@ class _WiFiPageState extends State<WiFiPage> {
   @override
   void dispose() {
     _SSIDcontroller.dispose();
+    _USERIDcontroller.dispose();
+    _PASSWDcontroller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        alignment: Alignment.topCenter,
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-                padding: const EdgeInsets.all(8.0),
-                alignment: Alignment.center,
-                child: Text('Please insert your WiFi Credentials below',
-                    style: GoogleFonts.montserrat(
-                      fontSize: 24,
-                    ))),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: TextField(
-                controller: _SSIDcontroller,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'SSID'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: const TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Username'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), labelText: 'Password'),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 40.0),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: buttonRoundBorder,
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 16.0, horizontal: 100.0)),
-                  onPressed: () {},
-                  child: Text('Save',
-                      style: Theme.of(context).textTheme.headline4!)),
-            )
-          ],
-        ),
+    return Form(
+      key: _WifiKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+              padding: const EdgeInsets.all(8.0),
+              alignment: Alignment.center,
+              child: Text('Please insert your WiFi Credentials below',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 24,
+                  ))),
+          TextFormField(
+            controller: _SSIDcontroller,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'SSID'),
+            validator: (text) {
+              if (text == null || text.isEmpty) {
+                return 'Please Enter an SSID';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+              controller: _USERIDcontroller,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(), labelText: 'Username'),
+              validator: (text) {
+                if (text == null || !text.isEmail()) {
+                  return 'Please Enter a Valid School ID';
+                }
+                return null;
+              }),
+          TextFormField(
+            controller: _PASSWDcontroller,
+            obscureText: true,
+            decoration: const InputDecoration(
+                border: OutlineInputBorder(), labelText: 'Password'),
+            validator: (text) {
+              if (text == null || text.isEmpty) {
+                return 'Please Enter a Password';
+              }
+              return null;
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 40.0),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: buttonRoundBorder,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 16.0, horizontal: 100.0)),
+                onPressed: () {
+                  if (_WifiKey.currentState!.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Save',
+                    style: Theme.of(context).textTheme.headline4!)),
+          )
+        ],
       ),
     );
   }
