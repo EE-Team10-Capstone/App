4f9774c3a81b4a02fe85046a9974a8bacf89ac94
NDCrocker <59457029+NDCrocker@users.noreply.github.com>
2022-03-23 19:35:14 -0600

    Trying soln for reading device services. Needing to change main.c in esp32-source to test

--- lib/providers/ble_provider.dart
+++ lib/providers/ble_provider.dart
@@ -6,37 +6,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'package:provider/provider.dart';
 
-// Initiating instances and global variable for services
-
-//Wifi Service and Characteristics
-
-//ThingSpeak Service and Characteristics
-
-// Bluetooth Connect Button for home page
-
 class BLE extends ChangeNotifier {
+// Initiating instances and global variable for services
   FlutterBlue fb = FlutterBlue.instance;
   late List<BluetoothService> services;
-
-  late String serviceuuid;
+  late BluetoothDevice ioTensor;
 
   bool isConnected = false;
 
-  // WiFi Characteristics
+//WiFi Service & Characteristics
   final String wifiUUID = "57694669-2050-726f-7669-73696f6e0000";
   final String ssidUUID = "57694669-2050-726F-7669-73696F6E0001";
   final String usidUUID = "57694669-2050-726F-7669-73696F6E0002";
   final String pswdUUID = "57694669-2050-726F-7669-73696F6E0003";
 
-// ThingSpeak Characteristics Insert when completed
+//ThingSpeak Service & Characteristics
   final String tsUUID = "54532050-726F-7669-7369-6F6E00000000";
   final String wrapiUUID = "54532050-726F-7669-7369-6F6E00000001";
 
+//Begin Sampling Service and Flag UUID
+  final String bsUUID = "";
+  final String flagUUID = "";
+
+// Bluetooth Connect Button for home page
   void scanBLE() {
     fb.scanResults.listen((List<ScanResult> results) {
       for (ScanResult result in results) {
         if (result.device.name == "UA-IOTENSR") {
           _connectBLE(result.device);
+          ioTensor = result.device;
         }
       }
     });
@@ -51,10 +49,11 @@ class BLE extends ChangeNotifier {
     } catch (error) {
       throw ("Device already connected");
     }
-    services = await device.discoverServices();
   }
 
   void wifiWrite(String ssid, String userid, String passwd) async {
+    services = await ioTensor.discoverServices();
+
     for (BluetoothService service in services) {
       if (service.uuid == Guid(wifiUUID)) {
         for (BluetoothCharacteristic characteristic
@@ -80,6 +79,7 @@ class BLE extends ChangeNotifier {
   void tsWrite(
       String
           wrapikey /*, String cochan, String tempchan, String rhchan*/) async {
+    services = await ioTensor.discoverServices();
     for (BluetoothService service in services) {
       if (service.uuid == Guid(tsUUID)) {
         for (BluetoothCharacteristic characteristic
@@ -87,18 +87,19 @@ class BLE extends ChangeNotifier {
           if (characteristic.uuid == Guid(wrapiUUID)) {
             characteristic.write(utf8.encode(wrapikey));
           }
-          // if (characteristic.uuid == Guid(usidUUID)) {
-          //   Future.delayed(const Duration(milliseconds: 2000), () {
-          //     characteristic.write(utf8.encode(userid));
-          //   });
-          // }
-          // if (characteristic.uuid == Guid(pswdUUID)) {
-          //   Future.delayed(const Duration(milliseconds: 1000), () {
-          //     characteristic.write(utf8.encode(passwd));
-          //   });
-          //}
         }
       }
     }
-  }
+  } // tsWrite end
+
+  // void beginSampling() {
+  //   for (BluetoothService service in services) {
+  //     if (service.uuid == Guid(bsUUID)) {
+  //       for (BluetoothCharacteristic characteristic
+  //           in service.characteristics) {
+  //             if
+  //           }
+  //     }
+  //   }
+  // }
 }
